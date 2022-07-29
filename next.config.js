bdcf803596b0c1c4650064482ac80f37beed8a21
suvincent <suvincent0226@gmail.com>
2022-07-29 22:20:28 +0800

    test2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== 'production';
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
   images: {
     loader: "custom",
   },
