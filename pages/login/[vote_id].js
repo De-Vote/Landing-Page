eb06f83c2a406921b27c25dfb44fcd4053dccbc6
suvincent <suvincent0226@gmail.com>
2022-10-31 11:19:54 +0800

    build fix

--- pages/login/[vote_id].js
+++ pages/login/[vote_id].js
@@ -2,7 +2,7 @@ import Login from "../../components/Vote/login";
 
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-export async function getStaticProps({ locale }) {
+export async function getServerSideProps({ locale }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, ['common', 'footer'])),
