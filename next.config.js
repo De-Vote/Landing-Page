b540228f28242b081acc14d3931c654f6f70ead7
suvincent <suvincent0226@gmail.com>
2022-11-03 01:29:32 +0800

    vercel platform fix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
 const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 const debug = process.env.NODE_ENV !== 'production';
