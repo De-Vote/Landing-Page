f45b16912100f0a9b9c8e67dcac1a28f9862d85a
suvincent <suvincent0226@gmail.com>
2022-11-20 11:55:24 +0800

    TallyResultModal顯示開票結果

--- components/Vote/tallyResultModal.js
+++ components/Vote/tallyResultModal.js
@@ -11,26 +11,34 @@ import votehelper from '../../lib/vote'
 export default function ResultModal(props) {
     const [show, setShow] = useState(false)
     const [questions, setQuestions] = useState([])
+    const [rates, setRates] = useState({})
     const router = useRouter()
 
     useEffect(() => {
         if (!router.isReady) return;
-        if(show)init()
+        if (show) init()
     }, [router.isReady, show])
 
-    async function init(){
+    async function init() {
         const { vote_id } = router.query
         const token = Cookies.get('token');
         let result = await votehelper.getVoteQuestion(token, vote_id)
         console.log(result.data.data)
-        if(result.data.data != null)setQuestions(result.data.data)
+        if (result.data.data != null) setQuestions(result.data.data)
+        if (result.data) {
+            let { active_rate, active_voters, total_voters, vote_rate, voted_voters } = result.data
+            let detail = {
+                active_rate, active_voters, total_voters, vote_rate, voted_voters
+            }
+            setRates(detail)
+        }
     }
 
-    async function tally(){
+    async function tally() {
         props.tally()
     }
 
-    function VoteEnded(){
+    function VoteEnded() {
         let t = (new Date(Date.parse(props.vote.end_time.replace(/-/g, '/'))))
         // return (new Date(t.getTime()+t.getTimezoneOffset()*60*1000)).toLocaleString()
         let now = new Date()
@@ -53,29 +61,34 @@ export default function ResultModal(props) {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    { props.vote.voting_status == 'Tally Ended' && questions ?
-                    <ol>
-                    {questions.map((question,index)=>{
-                        let detail = question.data.attributes
-                        console.log(detail)
-                        let counts = JSON.parse(detail.counts)
-                        let options = JSON.parse(detail.options)
-                        return <li key={index}>
-                            {detail.title}
-                            <ul>
-                                {options.map((option, i)=>
-                                    <li key={`options-${index}-${i}`}>{option} : {counts[i]}</li>
-                                )}
-                            </ul>
-                        </li>
-                    })}
-                    </ol>
-                    :(VoteEnded())? "Admin has not tally ballots!\n(管理員尚未開票)":"Vote has not ended!\n(投票尚未結束)"}
+                    {props.vote.voting_status == 'Tally Ended' && questions ?
+                        <>
+                            <h5>rate of active voters (領票率): {rates.active_rate * 100}%</h5>
+                            <h5>rate of voted voters (投票率): {rates.vote_rate * 100}%</h5>
+                            <h5>detail:</h5>
+                            <ol>
+                                {questions.map((question, index) => {
+                                    let detail = question.data.attributes
+                                    console.log(detail)
+                                    let counts = JSON.parse(detail.counts)
+                                    let options = JSON.parse(detail.options)
+                                    return <li key={index}>
+                                        {detail.title}
+                                        <ul>
+                                            {options.map((option, i) =>
+                                                <li key={`options-${index}-${i}`}>{option} : {counts[i]}</li>
+                                            )}
+                                        </ul>
+                                    </li>
+                                })}
+                            </ol>
+                        </>
+                        : (VoteEnded()) ? "Admin has not tally ballots!\n(管理員尚未開票)" : "Vote has not ended!\n(投票尚未結束)"}
                 </Modal.Body>
                 <Modal.Footer>
-                    {props.role == 'admin'?
-                        <Button variant="secondary" style={{ float: 'left' }} onClick={() => { tally()}}>Tally</Button>
-                    :""}
+                    {props.role == 'admin' ?
+                        <Button variant="secondary" style={{ float: 'left' }} onClick={() => { tally() }}>Tally</Button>
+                        : ""}
                 </Modal.Footer>
             </Modal>
         </>)
