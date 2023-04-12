23a83f1472031ff5b834ee743923719baf0639cf
Wayne <twe105@gapp.nthu.edu.tw>
2023-04-12 11:33:22 +0800

    add GA

--- components/Layout.js
+++ components/Layout.js
@@ -10,6 +10,15 @@ const Layout = (props) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       </Head>
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-E3JR90S2N3"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-E3JR90S2N3');
+      </script>
       <div>
         {props.children}
       </div>
