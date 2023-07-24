686d118d1bcc9584ba3d6e13f72e11efddbffa18
Wayne <twe105@gapp.nthu.edu.tw>
2023-07-24 23:49:00 +0800

    update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -47,7 +47,7 @@ const Footer = () => {
               
               <Col md={4}>
                 <h6 className="text-dark mb-3">Contact Us</h6>
-                <p className="text-muted f-14">General Building II Office 737, No.101, Sec. 2, Guangfu Rd., East Dist., Hsinchu City, Hsinchu, Hsinchu City, Taiwan 300</p>
+                <p className="text-muted f-14">General Building II Office 738, No.101, Sec. 2, Guangfu Rd., East Dist., Hsinchu City, Hsinchu, Hsinchu City, Taiwan 300</p>
                 <h6 className="text-muted pb-2">Email: contact@devote.tw</h6>
                 <ul className="list-unstyled footer-social-list mt-4">
                   <li className="list-inline-item"><a href="#"><i className="mdi mdi-facebook"></i></a></li>
