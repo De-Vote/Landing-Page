8d5f4550bb55c91b7093f354ae89d9567582647d
Wayne <twe105@gapp.nthu.edu.tw>
2023-07-24 23:23:05 +0800

    update Footer.js (add company chinese name)

--- components/Footer.js
+++ components/Footer.js
@@ -62,7 +62,7 @@ const Footer = () => {
         <Row className="mt-5">
           <Col md={12}>
             <div className="text-center text-muted">
-              <p className="mb-0 f-15">Copyright © 2022 De.Vote</p>
+              <p className="mb-0 f-15">Copyright © 2023 De.Vote 塊區投科技股份有限公司</p>
             </div>
           </Col>
         </Row>
