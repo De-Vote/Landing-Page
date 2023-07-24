9752c9ed85a11f8fc031535b47ba2afa231dafb8
Wayne <twe105@gapp.nthu.edu.tw>
2023-07-24 23:46:26 +0800

    update Layout.js, landing_page_index.json and landing_page_index.json

--- components/Layout.js
+++ components/Layout.js
@@ -5,7 +5,7 @@ const Layout = (props) => {
   return (
     <div>
       <Head>
-        <title>De.Vote</title>
+        <title>De.Vote 塊區投科技股份有限公司</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
