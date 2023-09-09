c541c5ee0996e359a7526328ead63726b42193ac
suvincent <suvincent0226@gmail.com>
2023-09-09 23:00:56 +0800

    bugfix: test routing asset

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   // i18n,
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '.' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${process.env.GHPAGE_ROUTE}` : '',
   images: {
     loader: "custom",
   },
